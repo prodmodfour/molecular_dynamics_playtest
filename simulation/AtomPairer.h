413ec1b875fa783a7e9ae5ca0d5a429181a0aaf8
prodmodfour <prodmodfour@gmail.com>
2025-01-17 15:14:58 +0000

    Finished all to all pairer

--- simulation/AtomPairer.h
+++ simulation/AtomPairer.h
@@ -49,15 +49,51 @@ class AlltoAllAtomPairer : public AtomPairer
 
             double r_cutoff = config.r_cutoff;
             double r_cutoff_squared = r_cutoff * r_cutoff;
+            double xi, yi, zi;
+            double xj, yj, zj;
+            double dx, dy, dz;
+            double squared_distance, distance;
+
             for (int i = 0; i < atoms.size(); i++)
             {
+                xi = atoms[i].x;
+                yi = atoms[i].y;
+                zi = atoms[i].z;
+                
                 for (int j = 0; j < atoms.size(); j++)
                 {
-                    atom_pairs.push_back(std::make_tuple(i, j, atoms[i].distance(atoms[j])));
+                    xj = atoms[j].x;
+                    yj = atoms[j].y;
+                    zj = atoms[j].z;
+
+                    dx = xj - xi;
+                    if (dx > r_cutoff)
+                    {
+                        continue;
+                    }
+                    dy = yj - yi;
+                    if (dy > r_cutoff)
+                    {
+                        continue;
+                    }
+                    dz = zj - zi;
+                    if (dz > r_cutoff)
+                    {
+                        continue;
+                    }
+
+                    squared_distance = dx * dx + dy * dy + dz * dz;
+                    if (squared_distance > r_cutoff_squared)
+                    {
+                        continue;
+                    }
+                    distance = sqrt(squared_distance);
+                    atom_pairs.push_back(std::make_tuple(i, j, distance));
                 }
             }
 
             return atom_pairs;
         }
+
 };
 
