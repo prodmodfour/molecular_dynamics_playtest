299d1ff0deca9cca9873717f1cbd2edb3eef5922
Ashraf <prodmodfour@gmail.com>
2025-02-04 03:51:56 +0000

    started demo 0

---
+++ simulation/demo_0.cpp
@@ -0,0 +1,23 @@
+
+
+
+// The purpose of this demo is to show how the MD Simulator works and to test whether it is working correctly.
+// Treat it both as a tutorial and a set of tests.
+
+// Test 1: Create a singe atom with no initial velocity. Watch it not move for 100 timesteps.
+
+// Test 2: Create a single atom with an initial velocity. Watch it move in a straight line at a constant velocity.
+
+// Test 3: Create a FCC crystal lattice and watch it be stable. No intiial velocities.
+
+// Test 4: Create a FCC crystal lattice and fire a slow moving particle at it. Watch it remain stable
+
+// Test 5: Create a FCC crystal lattice and fire a fast moving particle at it. Watch it explode. 
+
+
+
+
+
+
+
+
