82ecc1c50a3ca5b596e3983cd49ce245725951d1
prodmodfour <prodmodfour@gmail.com>
2025-02-07 16:18:56 +0000

    added encapsulation to structure class

--- simulation/Structure.h
+++ simulation/Structure.h
@@ -17,11 +17,39 @@ class Structure
             this->planes = planes;
             this->center = center;
         }
+    
+    std::vector<Atom> get_atoms()
+    {
+        return this->atoms;
+    }
 
+    std::string get_shape()
+    {
+        return this->shape;
+    }
+
+    std::vector<Vector> get_corners()
+    {
+        return this->corners;
+    }
+
+    std::map<std::string, Plane> get_planes()
+    {
+        return this->planes;
+    }
+
+    Vector get_center()
+    {
+        return this->center;
+    }
+
+    private:
         std::vector<Atom> atoms;
         std::string shape;
         std::vector<Vector> corners;
         std::map<std::string, Plane> planes;
+
+
         Vector center;
 
 
