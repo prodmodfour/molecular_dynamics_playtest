8306d67b8358c38248335d66fc1ec678a4bf7dc7
prodmodfour <prodmodfour@gmail.com>
2025-02-16 05:31:01 +0000

    updated tutorials

--- tutorials/basic_simulation/tutorial_1/tutorial_1.cpp
+++ tutorials/basic_simulation/tutorial_1/tutorial_1.cpp
@@ -21,39 +21,38 @@ int main(int argc, char *argv[])
     std::string atom_type = "Cu";
     double mass = 63.546;
     double radius = 1.28;
-    Atom atom(atom_type, mass, radius);
-    std::vector<Atom> atoms;
+    atoms::Atom atom(atom_type, mass, radius);
+    std::vector<atoms::Atom> atoms;
     atoms.push_back(atom);
 
 
     // Set up config for the simulation
     double r_cutoff = 0; // It's just one atom, so no cutoff is needed
     double timestep_size = 0.001;
-    Config config(r_cutoff, timestep_size);
+    simulation::Config config(r_cutoff, timestep_size);
 
 
-    // Create a MDSimulator
-    MDSimulator simulator;
 
 
     // Run the simulation
-    std::vector<Timestep> simulation_data;
+    std::vector<simulation::Timestep> simulation_data;
     int total_timesteps = 100;
+    double time = 0;
     for (int i = 0; i < total_timesteps; i++) 
     {
-        Timestep timestep = simulator.simulate_timestep(config, atoms);
+        simulation::Timestep timestep = simulation::simulate_timestep(config, atoms, time);
         simulation_data.push_back(timestep);
     }
 
     // Print the simulation data
-    for (Timestep timestep : simulation_data)
+    for (simulation::Timestep timestep : simulation_data)
     {
         std::cout << "Timestep: " << timestep.time << " " << "Atom position: " << timestep.atoms[0].x << ", " << timestep.atoms[0].y << ", " << timestep.atoms[0].z << std::endl;
     }
 
     // Launch the visualiser
     QApplication app(argc, argv);
-    MainWindow main_window(simulation_data);
+    ui::MainWindow main_window(simulation_data);
     main_window.show();
     app.exec();
 
--- tutorials/basic_simulation/tutorial_1/tutorial_6.cpp
+++ tutorials/basic_simulation/tutorial_1/tutorial_6.cpp
@@ -30,13 +30,15 @@ int main(int argc, char *argv[])
     double mass = 63.546;
     double radius = 1.28;
 
-    std::vector<Atom> atoms = generate_fcc(cubes_in_x, cubes_in_y, cubes_in_z, atom_spacing, atom_type, mass, radius);
+    std::vector<atoms::Atom> atoms = geometry::generate_atoms_in_fcc_pattern(cubes_in_x, cubes_in_y, cubes_in_z, 
+    atom_spacing, atom_type,
+     mass, radius);
 
     // Get dimensions of the box that contains the atoms
-    Box box(atoms);
+    geometry::Box box(atoms);
 
     // Create a moving atom that starts a set offset away from the center of the top place of the box
-    Atom moving_atom(atom_type, mass, radius);
+    atoms::Atom moving_atom(atom_type, mass, radius);
     double z_offset = 3;
     moving_atom.x = box.top_plane.center.x;
     moving_atom.y = box.top_plane.center.y;
@@ -56,31 +58,30 @@ int main(int argc, char *argv[])
     // Set up config for the simulation
     double r_cutoff = 4; 
     double timestep_size = 0.001;
-    Config config(r_cutoff, timestep_size);
+    simulation::Config config(r_cutoff, timestep_size);
 
 
-    // Create a MDSimulator
-    MDSimulator simulator;
-
+    
 
     // Run the simulation
-    std::vector<Timestep> simulation_data;
+    std::vector<simulation::Timestep> simulation_data;
     int total_timesteps = 100;
+    double time = 0;
     for (int i = 0; i < total_timesteps; i++) 
     {
-        Timestep timestep = simulator.simulate_timestep(config, atoms);
+        simulation::Timestep timestep = simulation::simulate_timestep(config, atoms, time);
         simulation_data.push_back(timestep);
     }
 
     // Print the simulation data
-    for (Timestep timestep : simulation_data)
+    for (simulation::Timestep timestep : simulation_data)
     {
         std::cout << "Timestep: " << timestep.time << " " << "Atom position: " << timestep.atoms[0].x << ", " << timestep.atoms[0].y << ", " << timestep.atoms[0].z << std::endl;
     }
 
     // Launch the visualiser
     QApplication app(argc, argv);
-    MainWindow main_window(simulation_data);
+    ui::MainWindow main_window(simulation_data);
     main_window.show();
     app.exec();
 
