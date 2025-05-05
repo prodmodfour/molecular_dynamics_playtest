da8e2ef9d6aefff9ad87e988059cf41021bbe483
Ashraf <prodmodfour@gmail.com>
2025-05-05 03:14:11 +0100

    Set all debug loggers to enabled

--- ui/settings_functions.cpp
+++ ui/settings_functions.cpp
@@ -14,6 +14,11 @@ void ui::load_settings()
     if (!developer_settings.contains("first_run") || developer_settings.value("first_run").toBool())
     {
         developer_settings.setValue("first_run", false);
+        developer_settings.setValue("ui_debug_logging_enabled", true);
+        developer_settings.setValue("simulation_debug_logging_enabled", true);
+        developer_settings.setValue("geometry_debug_logging_enabled", true);
+        developer_settings.setValue("physics_debug_logging_enabled", true);
+        developer_settings.setValue("atoms_debug_logging_enabled", true);
 
         developer_settings.sync();
     }
